7587b7a179ca8f2455b57ba1c740a0d76f509cf8
ss707494 <ss707494@163.com>
2017-04-03 10:24:16 +0800

    添加插件 CommonsChunkPlugin

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -224,6 +224,11 @@ module.exports = {
     ];
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      children: true,
+      minChunks: 3
+    }),
+
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
